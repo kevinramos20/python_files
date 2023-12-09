e8c69b07fdbb0b9e2bc7e4e221ed862f6d7b72a2
Kevin <kr758331@gmail.com>
2023-12-09 14:30:59 -0500

    first commit

---
+++ stack_high.py
@@ -0,0 +1,13 @@
+def stack_high(num1) :
+     if num1 == 0:
+        return num1
+     print ("current count:" ,num1)
+     num1 += 1
+     stack_high(num1)
+
+def main() :
+     num1 = stack_high (1)
+     print ("Current count:",num1)
+
+if __name__ == "__main__":
+     main()
